e004bca0af2385f9edbec44377c99e24ea6c13a4
ij66yjol <apurwa.agrawal@fau.de>
2023-06-30 20:45:12 +0200

    Structed the code a little more

--- sensation.py
+++ sensation.py
@@ -5,90 +5,109 @@ import datetime
 import threading
 import geocoder
 
-def listen_and_recognize():
-    r = sr.Recognizer()
-    engine = pyttsx3.init()
-    engine.setProperty("rate", 150)
+class Sensation:
+    def __init__(self):
+        self.r = sr.Recognizer()
+        self.engine = pyttsx3.init()
+        self.engine.setProperty("rate", 150)
+        self.sensation_active = False
 
-    sensation_active = False
+    def start(self):
+        self.print_and_speak("Welcome to Sensation. You can start speaking now.")
 
-    # Initial welcome message
-    print("Welcome to Sensation")
-    text_to_speech(engine, "Welcome to Sensation. You can start speaking now.")
+        while True:
+            try:
+                with sr.Microphone() as source:
+                    self.r.adjust_for_ambient_noise(source)
+                    audio = self.r.listen(source, timeout=5)
+                    result = self.r.recognize_google(audio)
+                    #print("You spoke:", result)
+                    self.print_and_speak("You said: " + result)
 
-    while True:
-        try:
-            with sr.Microphone() as source:
-                r.adjust_for_ambient_noise(source)
-                audio = r.listen(source, timeout=5)
-                result = r.recognize_google(audio)
-                print("You spoke:", result)
-                text_to_speech(engine, "You said: " + result)
-
-                if result.lower() == "start sensation":
-                    if not sensation_active:
-                        sensation_active = True
-                        print("Sensation started.")
-                        text_to_speech(engine, "Sensation started.")
-                elif result.lower() == "stop sensation":
-                    if sensation_active:
-                        sensation_active = False
-                        print("Sensation stopped.")
-                        text_to_speech(engine, "Sensation stopped.")
-                elif sensation_active:
-                    if "what" in result.lower() and "time" in result.lower():
-                        current_time = datetime.datetime.now().strftime("%H:%M")
-                        print("The current time is:", current_time)
-                        text_to_speech(engine, "The current time is " + current_time)
-                    elif "position" in result.lower() or "location" in result.lower():
-                        position = get_current_position()
-                        print("Current position:", position)
-                        text_to_speech(engine, "Your current position is " + position)
-                    elif "picture" in result.lower() or "photo" in result.lower():
-                        # take_picture()
-                        print("Picture was successfully stored.")
-                        text_to_speech(engine, "Picture was successfully stored.")
-                    elif "say hi to" in result.lower():
-                        name = result.lower().split("say hi to")[1].strip()
-                        print("Hello", name)
-                        text_to_speech(engine, "Hello " + name)
-                    elif result.lower() == "shut down sensation":
-                        print("Shutting down Sensation...")
-                        text_to_speech(engine, "Shutting down Sensation.")
-                        break
+                    if self.sensation_active:
+                        self.process_command(result)
                     else:
-                        text_to_speech(engine, "Invalid command.")
-                else:
-                    if result.lower() == "shut down sensation":
-                        print("Shutting down Sensation...")
-                        text_to_speech(engine, "Shutting down Sensation.")
-                        break
-                    else:
-                        print("Sensation is not active. Start Sensation to proceed.")
-                        text_to_speech(engine, "Sensation is not active. Start Sensation to proceed.")
+                        self.handle_inactive_sensation(result)
 
-        except sr.WaitTimeoutError:
-            pass
-        except sr.UnknownValueError:
-            pass
-        except sr.RequestError as e:
-            print("Error:", e)
+            except sr.WaitTimeoutError:
+                pass
+            except sr.UnknownValueError:
+                pass
+            except sr.RequestError as e:
+                print("Error:", e)
 
-def text_to_speech(engine, text):
-    engine.say(text)
-    engine.runAndWait()
+    def process_command(self, command):
+        if "what" in command.lower() and "time" in command.lower():
+            self.get_current_time()
+        elif "position" in command.lower() or "location" in command.lower():
+            self.get_current_position()
+        elif "picture" in command.lower() or "photo" in command.lower():
+            self.take_picture()
+        elif "say hi to" in command.lower():
+            name = command.lower().split("say hi to")[1].strip()
+            self.say_hello(name)
+        elif command.lower() == "stop sensation":
+            self.stop_sensation()
+        elif command.lower() == "shut down sensation":
+            self.shutdown()
+        else:
+            self.print_and_speak("Invalid command.")
 
-# Function to retrieve the current position
-def get_current_position():
-    g = geocoder.ip('me')
-    if g.ok:
-        city = g.city if g.city else ""
-        state = g.state if g.state else ""
-        country = g.country if g.country else ""
-        street = g.street if g.street else ""
-        return f"{street}, {city}, {state}"
-    else:
-        return "Unable to retrieve current position"
+    def handle_inactive_sensation(self, command):
+        if command.lower() == "start sensation":
+            if not self.sensation_active:
+                self.start_sensation()
+            else:
+                self.print_and_speak("Sensation is already active.")
+        elif command.lower() == "shut down sensation":
+            self.shutdown()
+        else:
+            self.print_and_speak("Sensation is not active. Start Sensation to proceed.")
 
-# Call the listen_and_recognize() function
-listen_and_recognize()
+    def start_sensation(self):
+        self.sensation_active = True
+        self.print_and_speak("Sensation started.")
+
+    def stop_sensation(self):
+        if self.sensation_active:
+            self.sensation_active = False
+            self.print_and_speak("Sensation stopped.")
+        else:
+            self.print_and_speak("Sensation is not active.")
+
+    def shutdown(self):
+        self.stop_sensation()
+        self.print_and_speak("Shutting down Sensation...")
+        # Additional cleanup or shutdown operations can be added here.
+        os._exit(0)
+
+    def print_and_speak(self, message):
+        print(message)
+        self.engine.say(message)
+        self.engine.runAndWait()
+
+    def get_current_time(self):
+        current_time = datetime.datetime.now().strftime("%H:%M")
+        self.print_and_speak("The current time is " + current_time)
+
+    def get_current_position(self):
+        g = geocoder.ip('me')
+        if g.ok:
+            city = g.city if g.city else ""
+            state = g.state if g.state else ""
+            country = g.country if g.country else ""
+            street = g.street if g.street else ""
+            position = f"{street}, {city}, {state}"
+        else:
+            position = "Unable to retrieve current position"
+        self.print_and_speak("Your current position is " + position)
+
+    def take_picture(self):
+        # Implementation for taking a picture can be added here
+        self.print_and_speak("Picture was successfully stored.")
+
+    def say_hello(self, name):
+        self.print_and_speak("Hello " + name)
+
+sensation = Sensation()
+sensation.start()
